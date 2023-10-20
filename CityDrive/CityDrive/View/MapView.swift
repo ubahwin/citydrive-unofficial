deb75e64794fd5a5824efcef32df675b1b4d923e
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-10-20 21:37:00 +0300

    rm: magic integers

--- CityDrive/CityDrive/View/MapView.swift
+++ CityDrive/CityDrive/View/MapView.swift
@@ -2,6 +2,10 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    let HEIGHT_SHEET = 260
+    let LAT_REGION = 200
+    let LON_REGION = 200
+    
     @StateObject private var mapVM = MapViewModel()
     @Environment(\.colorScheme) var colorScheme
     @State var camera: MapCameraPosition = .userLocation(fallback: .automatic)
@@ -53,16 +57,15 @@ struct MapView: View {
                 openCarDetail = new != nil
                 if let coordinate = carSelected?.placemark.coordinate {
                     camera = .region(
-        MKCoordinateRegion(center: coordinate, latitudinalMeters: 200, longitudinalMeters: 200)
+                        MKCoordinateRegion(center: coordinate, latitudinalMeters: CLLocationDistance(LAT_REGION), longitudinalMeters: CLLocationDistance(LON_REGION))
         )
                 }
             }
         }
         .sheet(isPresented: $openCarDetail) {
             CarView(car: $carSelected)
-                .presentationDetents([.height(260)])
-                .presentationBackgroundInteraction(.enabled(upThrough: .height(260)))
-                .presentationCornerRadius(10)
+                .presentationDetents([.height(CGFloat(HEIGHT_SHEET))])
+                .presentationBackgroundInteraction(.enabled(upThrough: .height(CGFloat(HEIGHT_SHEET))))
         }
     }
 }
