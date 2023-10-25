01b9e64bdde63ee3650a5f6451203edf3a5b179e
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-10-25 18:28:15 +0300

    feat: mapType for MKMapType

--- CityDrive/CityDrive/Model/Settings/MapType.swift
+++ CityDrive/CityDrive/Model/Settings/MapType.swift
@@ -5,7 +5,16 @@ enum MapType: String, CaseIterable, Identifiable {
     case hybrid, standard
     var id: Self { self }
 
+    /// Для Map из SwiftUI
     var mapStyle: MapStyle {
+        switch self {
+        case .hybrid: return .hybrid
+        case .standard: return .standard
+        }
+    }
+
+    /// Для императивной MKMapView
+    var mapType: MKMapType {
         switch self {
         case .hybrid: return .hybrid
         case .standard: return .standard
