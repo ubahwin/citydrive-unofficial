430412b1849060b0c5304dd689a435f0679ffb43
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-11-05 15:58:38 +0300

    feat: view controller for coordinate map

--- CityDrive/CityDrive/View/ImperativeMapView.swift
+++ CityDrive/CityDrive/View/ImperativeMapView.swift
@@ -1,137 +1,78 @@
 import Foundation
 import SwiftUI
 import MapKit
+import Combine
 
-struct ImperativeMapView: UIViewRepresentable {
+struct ImperativeMapView: UIViewControllerRepresentable {
     @ObservedObject var mapVM: MapViewModel
-    var locationManager = CLLocationManager()
 
-    class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: ImperativeMapView
-        @ObservedObject var mapVM: MapViewModel
-
-        init(parent: ImperativeMapView, mapVM: MapViewModel) {
-            self.parent = parent
-            self.mapVM = mapVM
-        }
+    func makeUIViewController(context: Context) -> MapViewController {
+        let mapVC = MapViewController()
+        mapVC.mapVM = mapVM
+        mapVC.mapView = MKMapView()
+        return mapVC
     }
 
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(parent: self, mapVM: mapVM)
+    func updateUIViewController(_ uiViewController: MapViewController, context: Context) {}
+}
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    var mapVM: MapViewModel!
+    var mapView: MKMapView!
+    private var cancellables: Set<AnyCancellable> = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mapSetup()
+
+        view = mapView
+
+        createSink()
     }
 
-    // TODO: do custom user location button with:
-    func goToUserLocation(_ mapView: MKMapView) {
-        if let userLocation = mapView.userLocation.location {
-            let region = MKCoordinateRegion(
-                center: userLocation.coordinate,
-                span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-            )
-            mapView.setRegion(region, animated: true)
-        }
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? ImperativeMapPin else { return nil }
+
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "ImperativeMapPin")
+        annotationView.image = annotation.image
+        annotationView.canShowCallout = true
+
+        return annotationView
     }
 
-    func makeUIView(context: Context) -> MKMapView {
-        let mapView = MKMapView()
-
-        updateMapType(mapView)
-
-        mapView.showsUserLocation = true
-        mapView.showsScale = true
-        mapView.showsUserTrackingButton = true
-
-        mapView.delegate = context.coordinator
-        return mapView
-    }
-
-    func updateMapType(_ mapView: MKMapView) {
-        mapView.mapType = mapVM.mapType?.mapType ?? .hybrid
-    }
-
-    func updateUIView(_ mapView: MKMapView, context: Context) {
-        if !mapVM.mapIsUpdate {
-            clearMap(mapView)
-
-            updateCars(mapView)
-            updateMapType(mapView)
-
-            mapVM.mapIsUpdate = true
-        }
-
-        if let userLocation = locationManager.location?.coordinate {
-            let region = MKCoordinateRegion(
-                center: userLocation,
-                span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-            )
-            mapView.setRegion(region, animated: true)
-        }
-    }
-
-    func clearMap(_ mapView: MKMapView) {
-        mapView.removeAnnotations(mapView.annotations)
-    }
-
-    func updateCars(_ mapView: MKMapView) {
+    func updateCars() {
         for car in mapVM.cars {
-            let pin = MKPointAnnotation()
-            pin.coordinate = car.coordinate
+            let pin = ImperativeMapPin(coordinate: car.coordinate)
             pin.title = car.model
             mapView.addAnnotation(pin)
         }
     }
-}
 
-class CustomAnnotation: NSObject, MKAnnotation {
-    var coordinate: CLLocationCoordinate2D
-    var title: String?
-    var subtitle: String?
-    var image: UIImage
+    func createSink() {
+        mapVM.$cars
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.updateCars()
+            }
+            .store(in: &cancellables)
+    }
 
-    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?, image: UIImage) {
-        self.coordinate = coordinate
-        self.title = title
-        self.subtitle = subtitle
-        self.image = image
+    func mapSetup() {
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.showsScale = true
+        mapView.showsUserTrackingButton = true
     }
 }
 
-// ================== SwiftUI (iOS 17) ==== КАРТА – ЛАГУЧИЙ КРИНЖ ==================
-//        ZStack {
-//            Map(
-//                position: $camera,
-//                interactionModes: mapVM.interactions,
-//                selection: $carSelected,
-//                scope: scope
-//            ) {
-//                ForEach(mapVM.currentCars) { car in
-//                    Annotation("", coordinate: car.coordinate) {
-//                        Pin(color: .green)
-//                    }
-//                }
-//                UserAnnotation {
-//                    Pin(color: .blue)
-//                }
-//            }
-//            VStack {
-//                HStack {
-//                    HStack {
-//                        BonusButtonView(mapVM: mapVM)
-//                        RefreshMapButtonView(mapVM: mapVM)
-//                    }
-//                    Spacer()
-//                }
-//                Spacer()
-//                HStack {
-//                    Spacer()
-//                    MapUserLocationButton(scope: scope)
-//                        .buttonBorderShape(.capsule)
-//                }
-//            }
-//            .padding()
+//    // TODO: do custom user location button with:
+//    func goToUserLocation(_ mapView: MKMapView) {
+//        if let userLocation = mapView.userLocation.location {
+//            let region = MKCoordinateRegion(
+//                center: userLocation.coordinate,
+//                span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+//            )
+//            mapView.setRegion(region, animated: true)
 //        }
-//        .mapStyle(mapVM.mapType?.mapStyle ?? MapStyle.standard)
-//        .mapScope(scope)
-//        .onMapCameraChange(frequency: MapCameraUpdateFrequency.onEnd) {
-//            mapVM.updateCurrentCars()
-//        }
-// ========================================================
+//    }
