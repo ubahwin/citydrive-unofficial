127b1b261611f079f537208e16e42ca90bdd3122
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-11-25 18:23:12 +0300

    feat(MapView): shadow on button

--- CityDrive/CityDrive/View/MapView.swift
+++ CityDrive/CityDrive/View/MapView.swift
@@ -5,31 +5,31 @@ struct MapView: View {
     @StateObject private var mapVM = MapViewModel()
 
     var body: some View {
-        VStack {
-            ZStack {
-                ImperativeMapView(mapVM: mapVM)
-                    .ignoresSafeArea()
-                    .onTapGesture {
-                        mapVM.openCarDetail = false
-                    }
-                VStack {
+        ZStack {
+            ImperativeMapView(mapVM: mapVM)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    mapVM.openCarDetail = false
+                }
+            VStack {
+                HStack {
                     HStack {
-                        HStack {
-                            BonusButtonView(mapVM: mapVM)
-                            RefreshMapButtonView(mapVM: mapVM)
-                        }
-                        Spacer()
-                        UserLocationButtonView(goToUser: $mapVM.goToUser)
+                        BonusButtonView(mapVM: mapVM)
+                        RefreshMapButtonView(mapVM: mapVM)
                     }
                     Spacer()
+                    UserLocationButtonView(goToUser: $mapVM.goToUser)
                 }
                 .padding()
+                .shadow(radius: 15)
+                Spacer()
             }
-            .sheet(isPresented: $mapVM.openCarDetail) {
-                CarView(mapVM: mapVM)
-                    .presentationDetents([.height(300)])
-                    .presentationBackgroundInteraction(.enabled(upThrough: .height(300)))
-            }
+        }
+        .sheet(isPresented: $mapVM.openCarDetail) {
+            CarView(mapVM: mapVM)
+                .presentationDetents([.height(300)])
+                .presentationBackgroundInteraction(.enabled(upThrough: .height(300)))
+                .presentationDragIndicator(.visible)
         }
     }
 }
