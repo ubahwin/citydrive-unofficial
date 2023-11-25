8ee484f563994cb7d286ad4277cdcfb276c0eacd
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-11-25 18:26:51 +0300

    SettingView: typos

--- CityDrive/CityDrive/View/SettingView.swift
+++ CityDrive/CityDrive/View/SettingView.swift
@@ -6,41 +6,42 @@ struct SettingView: View {
 
     var body: some View {
         NavigationStack {
-            List {
-                NavigationLink(destination: {
-                    UserInfoView(settingVM: settingVM)
-                }) {
-                    HStack {
-                        AsyncImage(url: URL(string: settingVM.user?.avatar ?? "")) { image in
-                            Avatar(image: image)
-                        } placeholder: {
-                            Avatar()
+            ZStack {
+                List {
+                    NavigationLink(destination: {
+                        UserInfoView(settingVM: settingVM)
+                    }) {
+                        HStack {
+                            AsyncImage(url: URL(string: settingVM.user?.avatar ?? "")) { image in
+                                Avatar(image: image)
+                            } placeholder: {
+                                Avatar()
+                            }
+                            VStack(alignment: .leading) {
+                                Text(settingVM.user?.firstName ?? "")
+                                Text(settingVM.user?.lastName ?? "")
+                            }
+                            .redacted(reason: settingVM.user == nil ? .placeholder : [])
+                            .bold()
+                            .padding(5)
+                            Spacer()
                         }
-                        VStack(alignment: .leading) {
-                            Text(settingVM.user?.firstName ?? "")
-                            Text(settingVM.user?.lastName ?? "")
+                        .allowsHitTesting(settingVM.user == nil)
+                    }
+                    Section("Карта") {
+                        Picker("Город", selection: settingVM.$city) {
+                            Text(City.spb.title).tag(City.spb)
+                            Text(City.moscow.title).tag(City.moscow)
+                            Text(City.sochi.title).tag(City.sochi)
                         }
-                        .redacted(reason: settingVM.user == nil ? .placeholder : [])
-                        .bold()
-                        .padding(5)
-                        Spacer()
+                        Picker("Тип карты", selection: settingVM.$mapType) {
+                            Text(MapType.standard.title).tag(MapType.standard)
+                            Text(MapType.hybrid.title).tag(MapType.hybrid)
+                        }
+                        MapInteractionsView(settingVM: settingVM)
                     }
-                    .allowsHitTesting(settingVM.user == nil)
+                    Toggle("Тёмная тема", isOn: settingVM.$isDarkMode)
                 }
-
-                Section("Карта") {
-                    Picker("Город", selection: settingVM.$city) {
-                        Text(City.spb.title).tag(City.spb)
-                        Text(City.moscow.title).tag(City.moscow)
-                        Text(City.sochi.title).tag(City.sochi)
-                    }
-                    Picker("Тип карты", selection: settingVM.$mapType) {
-                        Text(MapType.standard.title).tag(MapType.standard)
-                        Text(MapType.hybrid.title).tag(MapType.hybrid)
-                    }
-                    MapInteractionsView(settingVM: settingVM)
-                }
-                Toggle("Тёмная тема", isOn: settingVM.$isDarkMode)
             }
             .navigationTitle("Настройки")
         }
