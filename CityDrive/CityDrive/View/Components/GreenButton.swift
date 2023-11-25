80b1922c78f1e1a0a7f7c551ce0bab13d938e28f
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-11-25 18:09:42 +0300

    update(GreenButton): button frame on all width

--- CityDrive/CityDrive/View/Components/GreenButton.swift
+++ CityDrive/CityDrive/View/Components/GreenButton.swift
@@ -1,11 +1,19 @@
 import SwiftUI
 
 struct GreenButton: ButtonStyle {
+    var fixHeight: CGFloat?
+
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .padding()
-            .background(AppColor.green())
+            .padding(.vertical)
+            .frame(maxWidth: .infinity, maxHeight: fixHeight)            .background(AppColor.green())
             .clipShape(RoundedRectangle(cornerRadius: 10))
             .foregroundStyle(.white)
+            .padding()
     }
+}
+
+#Preview {
+    Button("asfd") { }
+        .buttonStyle(GreenButton(fixHeight: 50))
 }
