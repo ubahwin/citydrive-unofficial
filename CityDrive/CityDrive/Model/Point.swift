a9411506ac8b159a76dafc08b903c5b47114efec
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-11-25 18:06:27 +0300

    feat(Point): is Equatable

--- CityDrive/CityDrive/Model/Point.swift
+++ CityDrive/CityDrive/Model/Point.swift
@@ -1,11 +1,21 @@
 import Foundation
 import MapKit
 
-struct Point: Codable {
+struct Point: Codable, Equatable {
     var latitude: Double
     var longitude: Double
 
     var coordinate: CLLocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+
+    init(coordinate: CLLocationCoordinate2D) {
+        self.latitude = coordinate.latitude
+        self.longitude = coordinate.longitude
+    }
+
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
     }
 }
