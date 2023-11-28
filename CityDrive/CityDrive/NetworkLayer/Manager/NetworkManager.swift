451788bfff9a5887309b23217f1ffc6535f45864
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-11-28 10:26:00 +0300

    NetwManager: rm useless

--- CityDrive/CityDrive/NetworkLayer/Manager/NetworkManager.swift
+++ CityDrive/CityDrive/NetworkLayer/Manager/NetworkManager.swift
@@ -4,7 +4,6 @@ enum NetworkResponse: String {
     case success
     case authenticationError = "You need to be authenticated first."
     case badRequest = "Bad request"
-    case outdated = "The url you requested is outdated."
     case failed = "Network request failed."
     case noData = "Response returned with no data to decode."
     case unableToDecode = "We could not decode the response."
@@ -58,9 +57,9 @@ class NetworkManager {
     fileprivate func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String> {
         switch response.statusCode {
         case 200...299: return .success
+        case 400: return .failure(NetworkResponse.badRequest.rawValue)
         case 401...500: return .failure(NetworkResponse.authenticationError.rawValue)
         case 501...599: return .failure(NetworkResponse.badRequest.rawValue)
-        case 600: return .failure(NetworkResponse.outdated.rawValue)
         default: return .failure(NetworkResponse.failed.rawValue)
         }
     }
