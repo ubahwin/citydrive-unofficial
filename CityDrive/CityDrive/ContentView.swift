513980f4a33a42c18a1ce621be1ceae6d995fd1c
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-11-28 18:45:28 +0300

    feat: view model init in content view

--- CityDrive/CityDrive/ContentView.swift
+++ CityDrive/CityDrive/ContentView.swift
@@ -2,28 +2,36 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @StateObject private var mapVM = MapViewModel()
+    @StateObject private var orderVM = OrdersViewModel()
+    @StateObject private var settingVM = SettingViewModel()
+
     var body: some View {
-        TabView {
-            Group {
-                MapView()
-                    .tabItem {
-                        Image(systemName: "map")
-                        Text("Карта")
-                    }
-                OrderListView()
-                    .tabItem {
-                        Image(systemName: "list.bullet.clipboard")
-                        Text("История поездок")
-                    }
-                SettingView()
-                    .tabItem {
-                        Image(systemName: "gear")
-                        Text("Настройки")
-                    }
+        if mapVM.mapIsLoad {
+            TabView {
+                Group {
+                    MapView(mapVM: mapVM)
+                        .tabItem {
+                            Image(systemName: "map")
+                            Text("Карта")
+                        }
+                    OrderListView(orderVM: orderVM)
+                        .tabItem {
+                            Image(systemName: "list.bullet.clipboard")
+                            Text("История поездок")
+                        }
+                    SettingView(settingVM: settingVM)
+                        .tabItem {
+                            Image(systemName: "gear")
+                            Text("Настройки")
+                        }
+                }
+                .toolbarBackground(.visible, for: .tabBar)
             }
-            .toolbarBackground(.visible, for: .tabBar)
+            .accentColor(AppColor.green())
+        } else {
+            LoadingAppView()
         }
-        .accentColor(AppColor.green())
     }
 }
 
