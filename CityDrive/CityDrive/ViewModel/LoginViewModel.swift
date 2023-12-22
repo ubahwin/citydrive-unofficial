cdf16395c73e7da6a455d56475dcd0e94597414e
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-12-22 21:21:20 +0300

    chore

--- CityDrive/CityDrive/ViewModel/LoginViewModel.swift
+++ CityDrive/CityDrive/ViewModel/LoginViewModel.swift
@@ -2,11 +2,9 @@ import SwiftUI
 import SwiftKeychainWrapper
 
 class LoginViewModel: ObservableObject {
-    @Published var phone = ""
-    @Published var smsCode = ""
-
     @Published var isEntered = false
     @Published var next = false
+    private var phone = ""
 
     @AppStorage(Settings.isLogged) var isLogged: Bool?
 
@@ -20,7 +18,7 @@ class LoginViewModel: ObservableObject {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 
-    func sendSmsToPhone() {
+    func sendSmsToPhone(_ phone: String) {
         networkManager.sendPhone(phone: phone) { response, error in
             if let error = error {
                 print(error) // TODO: logging
@@ -28,6 +26,7 @@ class LoginViewModel: ObservableObject {
             }
 
             if let success = response?.success {
+                self.phone = phone
                 DispatchQueue.main.async {
                     self.next = success
                 }
@@ -35,7 +34,7 @@ class LoginViewModel: ObservableObject {
         }
     }
 
-    func enter() {
+    func enter(with smsCode: String) {
         networkManager.sendSms(phone: phone, smsCode: Int(smsCode) ?? 0) { response, error in // TODO: replace to VM
             if let error = error {
                 print(error) // TODO: logging
