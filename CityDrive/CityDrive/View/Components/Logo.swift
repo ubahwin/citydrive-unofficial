a23cc9eeb68fe10507022e185254adb646a58ce9
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-12-22 21:18:27 +0300

    add logo image component

--- CityDrive/CityDrive/View/Components/Logo.swift
+++ CityDrive/CityDrive/View/Components/Logo.swift
@@ -1,15 +1,11 @@
-//
-//  Logo.swift
-//  CityDrive
-//
-//  Created by Иван Вдовин on 22.12.2023.
-//
-
 import SwiftUI
 
 struct Logo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("logo")
+            .resizable()
+            .scaledToFit()
+            .frame(height: 40)
     }
 }
 
