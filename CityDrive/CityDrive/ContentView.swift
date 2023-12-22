9ba2b2eb885fcc1c445f6f15d31d0d36996235df
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-12-22 21:12:43 +0300

    feat(Map): double tap on tabbar => go to user

--- CityDrive/CityDrive/ContentView.swift
+++ CityDrive/CityDrive/ContentView.swift
@@ -6,6 +6,11 @@ struct ContentView: View {
     @StateObject private var orderVM = OrdersViewModel()
     @StateObject private var settingVM = SettingViewModel()
 
+    @State var selectedTab: Tab = .map
+    enum Tab: Hashable {
+        case map, orderList, settings
+    }
+
     var body: some View {
         if mapVM.mapIsLoad {
             TabView {
@@ -15,18 +20,31 @@ struct ContentView: View {
                             Image(systemName: "map")
                             Text("Карта")
                         }
+                        .tag(Tab.map)
                     OrderListView(orderVM: orderVM)
                         .tabItem {
                             Image(systemName: "list.bullet.clipboard")
                             Text("История поездок")
                         }
+                        .tag(Tab.orderList)
                     SettingView(settingVM: settingVM)
                         .tabItem {
                             Image(systemName: "gear")
                             Text("Настройки")
                         }
+                        .tag(Tab.settings)
                 }
                 .toolbarBackground(.visible, for: .tabBar)
+            }
+            .onTapGesture(count: 2) {
+                switch selectedTab {
+                case .map:
+                    mapVM.goToUser.toggle()
+                case .orderList:
+                    print("orderList")
+                case .settings:
+                    print("settings")
+                }
             }
             .accentColor(AppColor.green())
         } else {
---
+++ CityDrive/CityDrive/View/Components/Logo.swift
@@ -0,0 +1,18 @@
+//
+//  Logo.swift
+//  CityDrive
+//
+//  Created by Иван Вдовин on 22.12.2023.
+//
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Logo()
+}
